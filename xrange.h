163c02094e5f6b1905b0923f4b2de96e698c2a7a
Dhruv Paranjape <lord.dhruv@gmail.com>
2017-10-21 10:12:42 +0530

    update xrange making the iterator a subclass of xrange.

--- xrange.h
+++ xrange.h
@@ -1,25 +1,65 @@
 #ifndef _X_RANGE_H_
 #define _X_RANGE_H_
+#include <type_traits>
 
-#include "xrange_iter.h"
-
+template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 class xrange
 {
-    int _start;
-    int _end;
+    T _start;
+    T _end;
 
     public:
-        explicit constexpr xrange(const int end) : _start(0), _end(end) {}
-        constexpr xrange(int start, int end) : _start(start), _end(end) {}
-
-        constexpr xrange_iterator begin() const
+        explicit constexpr xrange(const T end) : _start(0), _end(end) {}
+        constexpr xrange(const T start, const T end) : _start(start), _end(end) {}
+        class iterator
         {
-            return xrange_iterator(_start, _end);
+            T value;
+            T end;
+
+            friend xrange<T>;
+
+            constexpr iterator(T _end) : value(_end), end(_end) {}
+            constexpr iterator(T _value, T _end) : value(_value), end(_end) {}
+            constexpr bool ended() const { return (value >= end); }
+
+            public:
+            #if __cpp_constexpr >= 201304
+                constexpr iterator& operator++() {
+            #else
+                iterator& operator++() {
+            #endif
+                    ++value;
+                    return *this;
+                }
+
+            #if __cpp_constexpr >= 201304
+                constexpr iterator operator++(T) {
+            #else
+                iterator operator++(T) {
+            #endif
+                    return iterator(value++, end);
+                }
+
+            constexpr bool operator==(const iterator& other) const {
+                return ((value == other.value && end == other.end) ||
+                        (ended() && other.ended()));
+            }
+
+            constexpr bool operator!=(const iterator& other) const {
+                return !(*this == other);
+            }
+
+            constexpr T operator*() const {
+                return value;
+            }
+        };
+
+        constexpr iterator begin() const {
+            return iterator(_start, _end);
         }
 
-        constexpr xrange_iterator end() const
-        {
-            return xrange_iterator(_end);
+        constexpr iterator end() const {
+            return iterator(_end);
         }
 };
 
